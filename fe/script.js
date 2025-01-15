d46f22663afe3920b9aefda5f376b5a3f7f7db03
ganeshgovala <sriramganesh28@gmail.com>
2025-01-15 19:11:41 +0530

    Developed Home page UI

--- fe/script.js
+++
@@ -1,7 +0,0 @@
-document.getElementById('form').addEventListener('submit', (e) => {
-    e.preventDefault(); // Prevent default form submission
-    const name = document.getElementById('name').value;
-    const email = document.getElementById('email').value;
-    console.log(name, email);
-    alert("Form submitted!");
-});
