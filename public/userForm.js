df1ecac0f16ccf04ecd8630367a58552144c418f
Gummala Sagar Sandeep Kumar <gummalasandy191212@gmail.com>
2025-07-24 22:18:37 +0530

    Update userForm.js

--- public/userForm.js
+++ public/userForm.js
@@ -21,63 +21,58 @@ async function getForm() {
       },
       body: JSON.stringify(data1),
     });
-    const promise = response.json();
-    promise
-      .then((res) => {
-        noOfQuestions = res.questions.length;
-        formField.innerHTML = "";
-        const header = document.createElement("div");
-        header.innerHTML = `
+    const docRef = doc(db, "Users", param1, "Forms", param2);
+    const docSnap = await getDoc(docRef);
+    if (docSnap.exists()) {
+      const res = docSnap.data();
+      noOfQuestions = res.questions.length;
+      formField.innerHTML = "";
+      const header = document.createElement("div");
+      header.innerHTML = `
                 <div class="h-16 bg-[#232323] rounded-t-xl mb-6"></div>
                 <h1 class="text-4xl font-semibold px-10">${res.title}</h1>
                 <p class="my-4 text-[#3f3f3f] px-10">${res.description}</p>
                 <hr />`;
-        formField.appendChild(header);
-        res.questions.forEach((element, index) => {
-          types[index] = {type : element.type, isImp : element.isImportant};
-          const questions = document.createElement("div");
-          questions.innerHTML = `<div class="mt-4 flex space-x-3 px-10">
-                <h1 class="text-xl font-semibold" id="question">${index+1}. ${element.question}</h1>
-                <p class="text-xl text-red-500" id="isImportant">${element.isImportant ? "*" : ""}</p>
-            </div>
-            <div class="px-14 mt-3 space-y-2">
-                ${element.type == "radio" || element.type == "checkbox" 
-                  ? ` <div class="space-x-2">
-                        <input type=${element.type} name="question${index}" id="option${index}1" value="1" class="size-4" />
-                        <label for="option${index}1" class="text-lg">${element.options[0]}</label>
-                      </div>
-                      <div class="space-x-2">
-                        <input type=${element.type} name="question${index}" id="option${index}2" value="2" class="size-4" />
-                        <label for="option${index}2" class="text-lg">${element.options[1]}</label>
-                      </div>
-                      <div class="space-x-2">
-                        <input type=${element.type} name="question${index}" id="option${index}3" value="3" class="size-4" />
-                        <label for="option${index}3" class="text-lg">${element.options[2]}</label>
-                      </div>
-                      <div class="space-x-2">
-                        <input type=${element.type} name="question${index}" id="option${index}4" value="4" class="size-4" />
-                        <label for="option${index}4" class="text-lg">${element.options[3]}</label>
-                      </div>`
-                  : `<input type=${element.type} class="mx-1 my-3 border-2 py-1 px-4 rounded-lg" id="question${index}">`
-                }
-            </div>
-            <hr class="mt-4">`;
-          formField.appendChild(questions);
-        })
-        const buttons = document.createElement("div");
-        buttons.innerHTML = `
+      formField.appendChild(header);
+      res.questions.forEach((element, index) => {
+        types[index] = {type : element.type, isImp : element.isImportant};
+        const questions = document.createElement("div");
+        let optionsHtml = "";
+        if (element.type === "radio" || element.type === "checkbox") {
+          for (let i = 0; i < 4; i++) {
+            optionsHtml += `
+              <div class="space-x-2">
+                <input type="${element.type}" name="question${index}" id="option${index}${i+1}" value="${i+1}" class="size-4" />
+                <label for="option${index}${i+1}" class="text-lg">${element.options[i]}</label>
+              </div>`;
+          }
+        } else {
+          optionsHtml = `<input type="${element.type}" class="mx-1 my-3 border-2 py-1 px-4 rounded-lg" id="question${index}">`;
+        }
+        questions.innerHTML = `
+          <div class="mt-4 flex space-x-3 px-10">
+            <h1 class="text-xl font-semibold" id="question">${index+1}. ${element.question}</h1>
+            <p class="text-xl text-red-500" id="isImportant">${element.isImportant ? "*" : ""}</p>
+          </div>
+          <div class="px-14 mt-3 space-y-2">
+            ${optionsHtml}
+          </div>
+          <hr class="mt-4">
+        `;
+        formField.appendChild(questions);
+      });
+      const buttons = document.createElement("div");
+      buttons.innerHTML = `
             <div class="mt-6 px-8 space-x-3">
                 <button class="py-2 px-8 bg-[#232323] text-white font-semibold rounded-xl" id="submit">Submit</button>  
             </div>
         `;
-        formField.appendChild(buttons);
-      })
-      .catch((err) => {
-        console.log(err);
-      });
+      formField.appendChild(buttons);
+    }
   } catch (err) {
     console.log(err);
   }
+      // ...existing code for dynamic HTML generation...
 }
 getForm();
 
@@ -124,22 +119,14 @@ async function addResponse() {
     responses : responses
   }
 
-  await fetch("http://localhost:3000/addResponse", {
-    method : "POST",
-    headers : {
-      "Content-Type" : "application/json"
-    },
-    body: JSON.stringify(data1)
-  })
-  .then((res) => {
-    res.text();
-  })
-  .catch((err) => window.alert("Can't Submit the form"));
-
-  console.log("submitted");
-
-  window.location.href = '/successfullPage';
-  window.history.replaceState(null, null, '/successfullPage');
+  try {
+    await addDoc(collection(db, "Users", param1, "Forms", param2, "Responses"), { response: responses });
+    console.log("submitted");
+    window.location.href = '/successfullPage';
+    window.history.replaceState(null, null, '/successfullPage');
+  } catch (err) {
+    window.alert("Can't Submit the form");
+  }
 }
 
 document.getElementById("formField").addEventListener("click", () => {
